3c3098bab0d30278b424d3945ba57e9f46e53a12
chite <105101021@nccu.edu.tw>
2019-12-29 17:38:31 +0800

    前置環境部屬完成

---
+++ src/component/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const SCSS_STYLE = require('./style.scss');
+import App from './App';
+ReactDOM.render(<App />, document.getElementById('root')); 

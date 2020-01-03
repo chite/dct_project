8f8e78b70c1160bdc0173adc54ec1c39a78e70ea
chite <105101021@nccu.edu.tw>
2020-01-04 00:07:17 +0800

    igHome 靜態刻板完成

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 import Login from './ig/Login';
 import Home from './ig/Home';
 
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
+            <HashRouter>
                 <Route path="/login" render={Login} />
                 <Route path="/home" render={Home} />
-            </BrowserRouter>
+            </HashRouter>
         )
     }
 }
--- src/component/ig/Home.jsx
+++ src/component/ig/Home.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import Bar from './share/Bar';
+import { IoMdGrid } from "react-icons/io";
+import { FaRegUserCircle, FaHeart, FaComment } from "react-icons/fa";
 import selfPt from '../../resources/selfie.jpg';
+import photo from '../../resources/igPhoto.jpg';
 
 function Home() {
     return (
         <div className="layout ig-bg">
             <Bar />
-            <article className="home-layout">
+            <article className="home-layout mb-5">
                 <div className="d-flex justify-content-between align-items-start pt-5">
                     <div className="self-photo" >
-                        <div className="photo mx-auto" style={{ 'backgroundImage': 'url(' + selfPt + ')' }}></div>
+                        <div className="photo-container mx-auto">
+                            <div className="photo" style={{ 'backgroundImage': 'url(' + selfPt + ')' }}></div>
+                        </div>
                     </div>
                     <div className="document">
                         <div className="d-flex flex-direction-row">
-                            <h1>k0000000</h1>
+                            <h1>pikachu111</h1>
                             <button className="btn text-white btn-sm ml-3">追蹤</button>
                             <button className="btn text-white btn-sm ml-3">&#x25BE;</button>
                             <h1 className="font-600 ml-3">&#183;&#183;&#183;</h1>
@@ -30,7 +35,65 @@ function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="history">
+                <div className="history mt-5">
+                    <div className="history-container">
+                        <div className="photo-container">
+                            <div className="bg history-photo" style={{ 'backgroundImage': 'url(' + selfPt + ')' }}></div>
+                        </div>
+                        <h6>AAAAAAAAAAAAAAAA</h6>
+                    </div>
+                    <div className="history-container">
+                        <div className="photo-container">
+                            <div className="bg history-photo" style={{ 'backgroundImage': 'url(' + selfPt + ')' }}></div>
+                        </div>
+                        <h6>AAAAAAAAAAAAAAAA</h6>
+                    </div>
+                    <div className="history-container">
+                        <div className="photo-container">
+                            <div className="bg history-photo" style={{ 'backgroundImage': 'url(' + selfPt + ')' }}></div>
+                        </div>
+                        <h6>AAAAAAAAAAAAAAAA</h6>
+                    </div>
+                </div>
+                <div className="post-banner d-flex justify-content-center mt-5">
+                    <div className="mx-5">
+                        <IoMdGrid />
+                        <span>&nbsp;貼文</span>
+                    </div>
+                    <div className="mx-5">
+                        <FaRegUserCircle />
+                        <span>&nbsp;已標註</span>
+                    </div>
+                </div>
+                <div className="cards">
+                    <div className="card-element">
+                        <div className="bg" style={{ 'backgroundImage': 'url(' + photo + ')' }}></div>
+                        <div className="icon">
+                            <FaHeart /><span>&nbsp;66</span>
+                            <FaComment /><span>&nbsp;2</span>
+                        </div>
+                    </div>
+                    <div className="card-element">
+                        <div className="bg" style={{ 'backgroundImage': 'url(' + photo + ')' }}></div>
+                        <div className="icon">
+                            <FaHeart /><span>&nbsp;66</span>
+                            <FaComment /><span>&nbsp;2</span>
+                        </div>
+                    </div>
+                    <div className="card-element">
+                        <div className="bg" style={{ 'backgroundImage': 'url(' + photo + ')' }}></div>
+                        <div className="icon">
+                            <FaHeart /><span>&nbsp;66</span>
+                            <FaComment /><span>&nbsp;2</span>
+                        </div>
+                    </div>
+                    <div className="card-element">
+                        <div className="bg" style={{ 'backgroundImage': 'url(' + photo + ')' }}></div>
+                        <div className="icon">
+                            <FaHeart /><span>&nbsp;66</span>
+                            <FaComment /><span>&nbsp;2</span>
+                        </div>
+                    </div>
 
                 </div>
             </article>

5c18ac1c7676172fd0b7f2fa4f88aff2abdeb2d0
chite <105101021@nccu.edu.tw>
2020-01-12 16:28:05 +0800

    lineHome 完成

--- src/component/line/LineHome.jsx
+++ src/component/line/LineHome.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaCommentDots, FaFileAlt, FaClock, FaSearch, FaUserPlus, FaRegUser, FaLine, FaRegSmile, FaLink, FaCrop } from "react-icons/fa";
+import { FaCommentDots, FaFileAlt, FaClock, FaSearch, FaUserPlus, FaRegUser, FaRegSmile, FaLink, FaCrop } from "react-icons/fa";
 import { FiUsers, FiUserPlus } from "react-icons/fi";
 import photo from '../../resources/igSelfie.jpg';
 
@@ -22,33 +22,107 @@ function LineHome() {
                 </nav>
                 <div className="container-fluid">
                     <div className="d-flex row bg-white">
-                        <div className="col-4 left-part d-flex flex-column justify-content-between">
-                            <div className="search-bar d-none d-sm-flex align-items-center">
+                        <div className="col-4 section d-flex flex-column justify-content-between">
+                            <div className="bar d-none d-sm-flex align-items-center">
                                 <FaSearch />
                                 <input placeholder="搜尋聊天室" readOnly />
                                 <h2>&#65049;</h2>
                             </div>
-                            <section className="friends-box">
-                                <div className="friend d-flex align-items-center pr-4">
+                            <section className="box">
+                                <div className="friend d-flex justify-content-between align-items-center px-4">
                                     <div className="img" style={{ 'backgroundImage': 'url(' + photo + ')' }}></div>
-                                    <div className="text ml-2">
+                                    <div className="text d-none d-sm-block ml-2">
                                         <h6>LINE TODAY</h6>
                                         <p>蔡英文發表勝選感言 「持續打造更好國家」</p>
                                     </div>
-                                    <div className="note d-flex flex-column align-items-end">
+                                    <div className="note d-none d-sm-flex flex-column align-items-end">
                                         <p className="time">AM 10:02</p>
-                                        <p className="number d-flex justify-content-center align-items-center">10</p>
+                                        <p className="number d-flex justify-content-center align-items-center">11</p>
                                     </div>
                                 </div>
+
                             </section>
-                            <div className="icon d-flex justify-content-between px-1 py-4">
+                            <div className="icon d-flex justify-content-between py-3 px-4">
                                 <FaCommentDots />
                                 <FaRegUser />
                                 <FiUsers />
                                 <FiUserPlus />
                             </div>
                         </div>
-                        <div className="col-8"></div>
+                        <div className="col-8 section d-flex flex-column justify-content-between">
+                            <div className="bar d-flex justify-content-between align-items-center">
+                                <div className="d-flex align-items-center">
+                                    <div className="img" style={{ 'backgroundImage': 'url(' + photo + ')' }}></div>
+                                    <h6>LINE TODAY</h6>
+                                </div>
+                                <h2>&#65049;</h2>
+                            </div>
+                            <section className="box">
+                                <div className="date my-3">
+                                    <div className="date-text">1.12（Sun）</div>
+                                </div>
+                                <div className="message d-flex align-items-end left sign">
+                                    <div className="content">
+                                        <span>蔡英文壓勝連任 日本5大報頭版伺候 雙殺韓國瑜 最快5月罷韓對決 注意保暖！ 北台灣轉涼高溫僅18度 海面一片血紅 292隻海龜集體死亡 開口要加薪好難？ 五技巧與叮嚀看這裡</span>
+                                    </div>
+                                    <div className="message-time">
+                                        <time>AM 10:02</time>
+                                    </div>
+                                </div>
+                                <div className="message d-flex align-items-end right sign">
+                                    <div className="message-time">
+                                        <time>AM 10:02</time>
+                                    </div>
+                                    <div className="content">
+                                        <span>蔡英文壓勝連任</span>
+                                    </div>
+                                </div>
+                                <div className="message d-flex align-items-end left">
+                                    <div className="img" style={{ 'backgroundImage': 'url(' + photo + ')' }}></div>
+                                    <div className="message-time">
+                                        <time>AM 10:02</time>
+                                    </div>
+                                </div>
+                                <div className="message d-flex align-items-end left">
+                                    <div className="img" style={{ 'backgroundImage': 'url(' + photo + ')' }}></div>
+                                    <div className="message-time">
+                                        <time>AM 10:02</time>
+                                    </div>
+                                </div>
+                                <div className="message d-flex align-items-end left">
+                                    <div className="img" style={{ 'backgroundImage': 'url(' + photo + ')' }}></div>
+                                    <div className="message-time">
+                                        <time>AM 10:02</time>
+                                    </div>
+                                </div>
+                                <div className="message d-flex align-items-end left">
+                                    <div className="img" style={{ 'backgroundImage': 'url(' + photo + ')' }}></div>
+                                    <div className="message-time">
+                                        <time>AM 10:02</time>
+                                    </div>
+                                </div>
+                                <div className="message d-flex align-items-end left">
+                                    <div className="img" style={{ 'backgroundImage': 'url(' + photo + ')' }}></div>
+                                    <div className="message-time">
+                                        <time>AM 10:02</time>
+                                    </div>
+                                </div>
+                                <div className="message d-flex align-items-end left">
+                                    <div className="img" style={{ 'backgroundImage': 'url(' + photo + ')' }}></div>
+                                    <div className="message-time">
+                                        <time>AM 10:02</time>
+                                    </div>
+                                </div>
+                            </section>
+                            <div className="icon d-flex justify-content-between py-3 px-4">
+                                <input placeholder="請輸入訊息。" readOnly />
+                                <div className="d-flex right-part align-items-center">
+                                    <FaRegSmile />
+                                    <FaLink />
+                                    <FaCrop />
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </article>
